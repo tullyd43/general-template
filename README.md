e7ac6ac0cbbaa661ea1af0d5496d352b2f7eccf8
David Tully <169837586+tullyd43@users.noreply.github.com>
2025-03-14 20:03:36 -0400

    Updated defaults and placeholders

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # general-template
+
+Create new repository from template on github
+git clone <url> to local repo folder
+
+run npm init -y
--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
-	entry: "./src/scripts/scripts.js",
+	entry: "./src/scripts/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
